73348d47d4b5aeee3e2e9737643b2e5cadad40e0
glitch404 <awsyf12238@gmail.com>
2026-03-05 11:54:58 +0200

    input schema validated & clients inputs processor created

--- src/core/client_request_processor.py
+++ src/core/client_request_processor.py
@@ -1,13 +1,19 @@
-from typing import Any
+import datetime as dt
+from typing import Any, Optional
+
+from src.util.utils import format_date
 
 
-def get_client_inputs(request: dict) -> tuple[Any, ...]:
-    start_urls: list[str] = request.get('links', [])
-    keywords: list[str] = request.get('keywords', [])
-    categories: list[str] = request.get('categories', ["latest"])
-    filter_fields: list[str] = request.get('filterFields', [])
-    max_articles: int = request.get('maxArticles', 100)
-    stop_date: str = request.get('stopDate', 'anytime')
-    proxy_cfg: dict = request.get('proxyCfg')
+def get_client_inputs(actor_input: dict) -> tuple[Any, ...]:
+    start_urls: Optional[list[dict]] = actor_input.get('links', [])
+    keywords: Optional[list[str]] = actor_input.get('keywords', [])
+    categories: list[str] = actor_input.get('categories', ["latest"])
+    filter_fields: Optional[list[dict]] = actor_input.get('filterFields', [])
+    max_articles: int = actor_input.get('maxArticles', 100)
+    stop_date: Optional[str] = actor_input.get('stopDate', 'anytime')
+    proxies: Optional[dict] = actor_input.get('proxyConfiguration', {"useApifyProxy": False})
 
-    return start_urls, keywords, categories, filter_fields, max_articles, stop_date, proxy_cfg
+    start_urls: list[str] = [url.get('url') for url in start_urls]
+    stop_date: dt.datetime = format_date(stop_date)
+
+    return start_urls, keywords, categories, filter_fields, max_articles, stop_date, proxies
--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
 from apify import Actor
+from typing import Any, Optional
+
+from src.settings import settings
 
 
 async def main() -> None:
@@ -12,20 +15,21 @@ async def main() -> None:
         # Process Actor input
         actor_input = await Actor.get_input() or {}
 
-        start_urls = [url.get('url') for url in actor_input.get('startUrls', LOCAL_DEFAULT_START_URLS)]
+        start_urls = [url.get('url') for url in actor_input.get('startUrls', [])]
         date_range = actor_input.get('dateRange', None)
         max_articles = actor_input.get('maxArticles', 100)
-        proxy_configuration = actor_input.get('proxyConfiguration')
+        proxy_configuration: Optional[dict] = actor_input.get('proxyConfiguration', {"useApifyProxy": False})
+        await Actor.set_status_message('Configuring proxies if any were provided...')
+        proxy_configuration: Any = await Actor.create_proxy_configuration(actor_proxy_input=proxy_configuration)
+        if proxy_configuration:
+            proxies: list = [proxy_configuration.new_url() for _ in range(len(start_urls))]
+            if isinstance(proxies, list): settings['PROXIES'] = proxies
+
         Actor.log.info(f'\nstart urls: {start_urls}\nmax articles: {max_articles}\ndate range: {date_range}\n')
 
-        # Get Scrapy project settings with custom configurations
         if date_range:
             date_range = date_range_handle(date_range.lower().replace(" ", "").replace('s', ''))
         settings = __get_scrapy_settings(start_urls, int(max_articles), date_range, proxy_configuration)
 
-        # Execute the spider using Scrapy CrawlerProcess
         Actor.log.info('Crawling Started...')
 
-        process = CrawlerProcess(settings)
-        process.crawl(Spider)
-        process.start()
