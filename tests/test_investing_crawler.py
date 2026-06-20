2412b807f582549d612ba31c97f6bf009ae397b8
G-Glitch404 <awysf1238@gmail.com>
2026-06-20 20:50:43 +0300

    add 1 test file with 2 tests init

---
+++ tests/test_investing_crawler.py
@@ -0,0 +1,35 @@
+from src.api.investing_api import InvestingAPI
+
+max_articles: int = 10
+categories: list[str] = ["latest-news"]
+links: list[str] = ["https://www.investing.com/news/latest-news/1"]
+
+
+def test_scrape_links() -> None:
+    crawler = InvestingAPI()
+    articles_counter: int = 0
+    for link in links:
+        if articles_counter >= max_articles: break
+        for article in crawler.crawl_page(link=link):
+            if articles_counter >= max_articles: break
+            articles_counter += 1
+
+            for k, v in article:
+                print(f"{k}: {v}")
+
+    assert articles_counter == max_articles
+
+
+def test_scrape_categories() -> None:
+    crawler = InvestingAPI()
+    articles_counter: int = 0
+    for category in categories:
+        if articles_counter >= max_articles: break
+        for article in crawler.crawl(topic_category=category, max_articles=max_articles):
+            if articles_counter >= max_articles: break
+            articles_counter += 1
+
+            for k, v in article:
+                print(f"{k}: {v}")
+
+    assert articles_counter == max_articles
